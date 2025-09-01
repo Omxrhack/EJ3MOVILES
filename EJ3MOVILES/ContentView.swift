08ea84b77837328053d7311cff3286cc8a452cec
Omar Bermejo Osuna <l21170203@culiacan.tecnm.mx>
2025-08-31 22:40:50 -0700

    Initial Commit

---
+++ EJ3MOVILES/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EJ3MOVILES
+//
+//  Created by Omar Bermejo Osuna on 31/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EJ3MOVILES/EJ3MOVILESApp.swift
@@ -0,0 +1,17 @@
+//
+//  EJ3MOVILESApp.swift
+//  EJ3MOVILES
+//
+//  Created by Omar Bermejo Osuna on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct EJ3MOVILESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
